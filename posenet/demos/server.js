99f105c0308aacba002f5b30bf87e1c5b945915a
Margaret Minsky <marg@media.mit.edu>
2019-06-03 21:52:53 -0400

    Working OSC UDP client

--- posenet/demos/server.js
+++ posenet/demos/server.js
@@ -1,11 +1,27 @@
 const WebSocket = require('ws');
+const osc = require("osc");
 
 const wss = new WebSocket.Server({ port: 8080 });
+var oscPort = new osc.UDPPort({
+    remoteAddress: "127.0.0.1",
+    remotePort: 6448,
+    metadata: true
+});
+oscPort.open();
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    oscPort.send({
+       address: "/wek/inputs",
+       args: [
+           {
+               type: "f",
+               value: 440
+           }
+       ]
+   });
   });
 
-  ws.send('something');
+
 });
